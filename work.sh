36a84ad470779d1c2de2d6a0d964e576d22d575c
Xavier Via <fernando.via@gmail.com>
2014-11-15 21:51:06 +0100

    + ‘work.sh’

---
+++ work.sh
@@ -0,0 +1,5 @@
+alias npm-exec='PATH=$(npm bin):$PATH'
+alias kco="cd ~/Code/checkout"
+alias ac="ack --pager='less -R'"
+alias nordic="cd ~/Code/kco-nordics"
+alias pull-locale="bundle exec localeapp pull"
