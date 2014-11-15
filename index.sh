c615e7db78f9c17204da5c06c35706a53abef102
Xavier Via <fernando.via@gmail.com>
2014-11-15 22:03:43 +0100

    / ‘index.sh’

--- index.sh
+++ index.sh
@@ -1,11 +1,11 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 source git-aliases.sh
-$DIR/ø.sh
-$DIR/boot2docker.sh
-$DIR/nvm.sh
-$DIR/rvm.sh
-$DIR/simple-server.sh
-$DIR/work.sh
-$DIR/fbr.sh
-$DIR/en_US.sh
+# $DIR/ø.sh
+# $DIR/boot2docker.sh
+# $DIR/nvm.sh
+# $DIR/rvm.sh
+# $DIR/simple-server.sh
+# $DIR/work.sh
+# $DIR/fbr.sh
+# $DIR/en_US.sh
