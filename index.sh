d2aba27fd2e0d8181e53ec59d7f7818454810c13
Xavier Via <fernando.via@gmail.com>
2014-11-15 21:25:54 +0100

    - ‘SparkleShare.txt’ / ‘index.sh’

--- index.sh
+++ index.sh
@@ -1,2 +1,19 @@
 sh git-aliases.sh
 sh ø.sh
+
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
+
+alias server="python -m SimpleHTTPServer"
+
+export NVM_DIR="/Users/fernando.canel/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+
+export PATH="$PATH:/Applications/ADT/sdk/tools:/Applications/ADT/platform-tools"
+
+ulimit -n 4096
+ulimit -s 32768
+
+archey
