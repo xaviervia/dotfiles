645f79917147a0270af70f70181c1a99f84a76ee
Xavier Via <fernando.via@gmail.com>
2014-11-15 21:58:09 +0100

    / ‘index.sh’

--- index.sh
+++ index.sh
@@ -9,20 +9,3 @@ $DIR/simple-server.sh
 $DIR/work.sh
 $DIR/fbr.sh
 $DIR/en_US.sh
-
-export LC_ALL=en_US.UTF-8
-export LANG=en_US.UTF-8
-
-export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
-
-alias server="python -m SimpleHTTPServer"
-
-export NVM_DIR="/Users/fernando.canel/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
-
-export PATH="$PATH:/Applications/ADT/sdk/tools:/Applications/ADT/platform-tools"
-
-ulimit -n 4096
-ulimit -s 32768
-
-archey
