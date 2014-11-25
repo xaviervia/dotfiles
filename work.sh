a9f0d6799774dfcf1705af3a678531041bc0fcdb
Xavier Via <fernando.via@gmail.com>
2014-11-25 15:06:28 +0100

    / ‘work.sh’

--- work.sh
+++ work.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
 alias npm-exec='PATH=$(npm bin):$PATH'
-alias kco="cd ~/Code/checkout"
 alias ac="ack --pager='less -R'"
-alias nordic="cd ~/Code/kco-nordics"
-alias otp="cd ~/Code/otp"
 alias pull-locale="bundle exec localeapp pull"
