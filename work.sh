5af9226204f236d0b2156e49c8f0d4f9d8189392
Xavier Via <fernando.via@gmail.com>
2014-11-26 10:12:08 +0100

    / ‘work.sh’

--- work.sh
+++ work.sh
@@ -3,3 +3,5 @@
 alias npm-exec='PATH=$(npm bin):$PATH'
 alias ac="ack --pager='less -R'"
 alias pull-locale="bundle exec localeapp pull"
+
+export PATH=$PATH:/Applications/ADT/sdk/platform-tools:/Applications/ADT/sdk/tools
