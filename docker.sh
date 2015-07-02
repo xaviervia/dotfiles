74aade65ab82c3cf8d0811c0341c804b61e091ba
Xavier Via <fernando.via@gmail.com>
2015-07-02 12:20:23 +0200

    / ‘docker.sh’

--- docker.sh
+++ docker.sh
@@ -3,5 +3,5 @@ alias have-you-tried-turning-it-off-and-on-again='docker rm -f $(docker ps -aq)'
 alias machine="docker-machine"
 
 switch() {
-  eval "machine env $1"
+  eval 'eval "machine env $1"'
 }
