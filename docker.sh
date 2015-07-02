aee5cc2882435615a4c0ddd2a2155629bbc16f68
Xavier Via <fernando.via@gmail.com>
2015-07-02 12:19:29 +0200

    / ‘docker.sh’

--- docker.sh
+++ docker.sh
@@ -1,2 +1,7 @@
 alias compose="docker-compose"
 alias have-you-tried-turning-it-off-and-on-again='docker rm -f $(docker ps -aq)'
+alias machine="docker-machine"
+
+switch() {
+  eval "machine env $1"
+}
