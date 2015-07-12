1f90a2ca7d3d7dd605e6b2a4f5eab1063be27431
Xavier Via <fernando.via@gmail.com>
2015-07-12 21:19:08 +0200

    / ‘docker.sh’

--- docker.sh
+++ docker.sh
@@ -1,5 +1,6 @@
 alias compose="docker-compose"
 alias have-you-tried-turning-it-off-and-on-again='docker rm -f $(docker ps -aq)'
+alias reclaim-disk-space='docker rmi $(docker images | grep "^<none>" | awk "{print $3}")'
 alias machine="docker-machine"
 
 switch() {
