a1ff895b6e6d50190ec19b1e1573ac1c969aaa33
Xavier Via <fernando.via@gmail.com>
2015-08-21 13:30:19 +0200

    / ‘docker.sh’

--- docker.sh
+++ docker.sh
@@ -2,6 +2,7 @@ alias compose="docker-compose"
 alias have-you-tried-turning-it-off-and-on-again='docker rm -f $(docker ps -aq)'
 alias reclaim-disk-space='docker images -q |xargs docker rmi'
 alias machine="docker-machine"
+alias dockito-vault="docker run -d -p 172.17.42.1:14242:3000 -v ~/.ssh:/vault/.ssh dockito/vault"
 
 switch() {
   eval "$(machine env $1)"
