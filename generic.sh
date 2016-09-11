ad6c3dd41f1bcf44cc5e2077a2c811f529a71afa
Xavier Via <fernando.via@gmail.com>
2016-09-11 22:30:31 +0200

    Add magical ed

--- generic.sh
+++ generic.sh
@@ -3,3 +3,4 @@ alias tree="tree -Cla -L 3"
 alias only="ls | grep"
 alias xavier="git config user.name 'Xavier Via' && git config user.email 'fernando.via@gmail.com'"
 alias baires="TZ=America/Buenos_Aires date"
+alias t='ed -p"🚀  "'
